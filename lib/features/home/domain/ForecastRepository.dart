acb3bbc3280e6ebf2e2f4f3e9a3db5b7fc74eace
Muhammed <muhammedrefaat2020@gmail.com>
2024-10-19 00:28:36 +0300

    uploading forcast branch to remote

--- lib/features/home/domain/ForecastRepository.dart
+++ lib/features/home/domain/ForecastRepository.dart
@@ -1,6 +1,6 @@
 import '../data/models/fetch_forecast_response.dart';
 import 'entity/Forecast.dart';
 
-abstract class ForecastRepository {
+abstract class ForecastRepository { sada
   Future<FetchForecastEntity> getForecast(String city);
 }
